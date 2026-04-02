62c9d8f4e1a6b39cdf13c3fdca09285b438d7d9a
Colton <coltonselby1@gmail.com>
2026-04-01 21:49:53 -0400

    added item framework

--- scripts/clicker.js
+++ scripts/clicker.js
@@ -36,7 +36,7 @@ class Upgrade {
         this.count = 0;
         this.baseCost = baseCost;
         this.cost = baseCost;
-        this.unlocked = unlocked;
+        this.unlocked = unlocked ?? false;
     }
 
     buy() {
@@ -62,12 +62,18 @@ const upgrades = [
     new Upgrade("Helmsman", 781250, 1000),
     new Upgrade("Cook", 3875000, 5000),
     new Upgrade("Swordsman", 18750000, 10000)
-]
+];
 
 function checkUnlocks() {
     for (let index = 0; index < upgrades.length - 1; ++index) {
         if (upgrades[index].count >= 10 ) upgrades[index+1].unlocked = true;
     }
+}
+
+function getIndexOfCurrentUpgrade() {
+for (let index = upgrades.length - 1; index >= 0; --index) {
+    if (upgrades[index].unlocked) return index;
+}
 }
 
 function getIdleDubloons() {
@@ -82,12 +88,87 @@ function getIdleDubloons() {
 
 class Item { 
 
-    constructor(name, tier, type, value) {
+    constructor(name, tier, type, value, consumable) {
         this.name = name;
         this.tier = tier;
         this.type = type;
         this.value = value;
+        this.consumable = consumable ?? false;
     }
+}
+
+const itemNames = ['Compass', 'Treasure Map', 'Cannon', 'Lucky Dice', 'Jolly Roger', 'Straw Hat', 'Spyglass', 'Sword', 'Ancient Text', 'Slingshot', 'Meat', 'Rum Barrel', 'Medicine', 'Powder Barrel', 'Dubloons']
+
+const itemTypes = ['dpcFlat', 'dpcMult', 'dpsFlat', 'spinLuck', 'dpsMult', 'heldMult', 'spinLuck', 'critChance', 'recruitmentDiscount', 'critMult', 'dpsBurst', 'tempDpsMult', 'tempDpcMult', 'tempDpcMult', 'instantDubloons']
+
+const itemValues = [
+    [1, 3, 8, 25, 75],
+    [1.25, 1.5, 2, 3, 5],
+    [2, 8, 30, 120, 500],
+    [1, 2, 3, 4, 5],
+    [1.05, 1.1, 1.2, 1.35, 1.5], 
+    [1.05, 1.1, 1.2, 1.35, 1.5],
+    [1, 2, 3, 4, 5], 
+    [0.05, 0.08, 0.12, 0.18, 0.25],
+    [0.05, 0.1, 0.18, 0.28, 0.4],
+    [1.5, 2, 3, 4.5, 7],
+    [0.5, 1, 2, 4, 8],
+    [1.5, 2, 2.5, 3, 5],
+    [1.5, 2, 2.5, 3, 5],
+    [2, 4, 8, 15, 30],
+    [50, 300, 2000, 30000, 500000]
+]
+
+const items = [];
+
+for (let i = 0; i < itemNames.length; ++i) {
+    let itemRow = [];
+    for (let j = 0; j < itemValues[i].length; ++j) {
+        if (i > 9) itemRow.push(new Item(itemNames[i], j, itemTypes[i], itemValues[i][j], true));
+        else itemRow.push(new Item(itemNames[i], j, itemTypes[i], itemValues[i][j]));
+    }
+    items.push(itemRow);
+}
+
+function getSlotCount() {
+    let slots = 3;
+    slots += getIndexOfCurrentUpgrade();
+    return slots;
+}
+
+const inventory = [null, null, null, null, null, null, null, null, null, null, null, null]
+
+function getSpinWeights() {
+    const weights = [50, 30, 15, 4, 1]
+    const maxTier = Math.floor(getIndexOfCurrentUpgrade() / 2);
+    for (let index = 0; index < weights.length; ++index) {
+        if (index > maxTier) weights[index] = 0;
+    }
+    inventory.filter(e => e !== null).forEach(element => {
+        if (element.type === 'spinLuck') {
+            for (let index = 0; index < weights.length; index++) {
+                if (index < 2) weights[index] -= element.value;
+                if (index > 2) weights[index] += element.value;
+                if (weights[index] < 0) weights[index] = 0;
+            }
+        }
+    });
+    return weights;
+}
+
+function rollRarity() {
+    const weights = getSpinWeights();
+    const total = weights.reduce((sum, w) => sum + w, 0);
+    let roll = Math.random() * total;
+    for (let i = 0; i < weights.length; i++) {
+        roll -= weights[i];
+        if (roll <= 0) return i;
+    }
+}
+
+function rollItem() {
+    let i = Math.floor(Math.random() * items.length);
+    return items[i][rollRarity()];
 }
 
 // ---- DOM UPDATE ----
@@ -117,7 +198,7 @@ function drawUpgrade(index) {
         if (!bought) return;
         updateUpgradeText();
         updateUpgrade(index);
-        if (upgrades[index].count === 10 && index < 9) drawUpgrade(index + 1);
+        if (upgrades[index].count === 10 && index != 9) drawUpgrade(index + 1);
         updateDubloonText();
         localStorage.setItem('dubloons', dubloons.toString());
         localStorage.setItem('upgrades', JSON.stringify(upgrades));
